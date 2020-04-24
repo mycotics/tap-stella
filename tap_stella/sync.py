a1390c0e1affedb1f6f1c66c6bbf021cd07a1710
Paul Harper <paul@pathlight.com>
2020-04-24 15:07:46 -0700

    Fix error getting the qa['scorecard']['archived_at'] field to match the schema

--- tap_stella/sync.py
+++ tap_stella/sync.py
@@ -1,8 +1,20 @@
 import singer
+import datetime
 
 from .client import Client
 
 LOGGER = singer.get_logger()
+
+
+def deep_get(d: dict, key_path: tuple):
+    key_path = key_path.split('.')
+    value = d
+    for key in key_path:
+        if isinstance(value, dict):
+            value = value.get(key)
+        else:
+            return None
+    return value
 
 
 def sync_qa(client, stream, state):
@@ -16,6 +28,18 @@ def sync_qa(client, stream, state):
         for row in rows:
             if row['score']:
                 row['score'] = float(row['score'].strip('%'))/100
+
+            if isinstance(deep_get(row, 'scorecard.archived_at'), dict):
+                # The schema expects a string here for this field, but we've
+                # been getting json serializations of the ruby on rails type
+                # ActiveSupport::TimeWithZone. This converts to string.
+                archived_at = deep_get(row, 'scorecard.archived_at.utc.^t')
+                if archived_at:
+                    archived_at = datetime.datetime.fromtimestamp(
+                        archived_at, datetime.timezone.utc
+                    ).isoformat()
+                row['scorecard']['archived_at'] = archived_at
+
         # write one or more rows to the stream:
         singer.write_records(stream.tap_stream_id, rows)
         singer.write_state({stream.tap_stream_id: new_bookmark})
